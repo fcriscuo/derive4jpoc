d6d67ca3c4f121a8976ff61fb44242e8595de1cb
Fred Criscuolo <fjcriscuolo@ucsd.edu>
2015-10-26 04:22:43 -0400

    More Validation and Try examples

--- src/main/java/edu/jhu/fcriscu1/functionaljava/TryDemo.java
+++ src/main/java/edu/jhu/fcriscu1/functionaljava/TryDemo.java
@@ -1,8 +1,13 @@
 package edu.jhu.fcriscu1.functionaljava;
 
+import fj.F;
 import fj.Try;
 import fj.data.Validation;
+import fj.function.Longs;
+import fj.function.Strings;
+import fj.function.Try0;
 import fj.function.Try1;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.util.Random;
@@ -19,6 +24,36 @@ public class TryDemo {
         }else {
             logger.info("Success: " +v1.success());
         }
+
+        Validation<Exception,Integer> v2 = validParseString("A");
+        if(v2.isFail()){
+            logger.error(v2.fail().getMessage());
+        } else {
+            logger.info("Inetger value= " + v2.success());
+        }
+        Validation<Exception,Integer> v3 = validParseString("123");
+        if(v3.isFail()){
+            logger.error(v3.fail().getMessage());
+        } else {
+            logger.info("Inetger value= " + v3.success());
+        }
+        String testName1 = "Fred";
+        Validation<Exception,Boolean>  v4 = isValidName(testName1);
+        if(v4.isFail()){
+            logger.error(v4.fail().getMessage());
+        } else {
+            logger.info(testName1 +" is valid name: "+v4.success());
+        }
+        String testName2 = "";
+        Validation<Exception,Boolean>  v5 = isValidName(testName2);
+        if(v5.isFail()){
+            logger.error(v5.fail().getMessage());
+        } else {
+            logger.info(testName2 +" is valid name: "+v5.success());
+        }
+
+        logger.info( Longs.multiply.f(100L).f(200L));
+
     }
     static Validation<Exception,Integer> getExternalValue() {
         Try1<Random, Integer,Exception> t = r -> {
@@ -31,8 +66,37 @@ public class TryDemo {
             } else {
                 return i;
             }
+
         };
         return Try.f(t).f(new Random());
     }
+
+    static Validation<Exception,Integer>  validParseString(String anInt)
+    {
+       if(!StringUtils.isNumeric(anInt)){
+           return Validation.fail( new NumberFormatException("Number format")) ;
+       }
+        if (StringUtils.isEmpty(anInt)){
+            return Validation.fail( new IllegalArgumentException("Null or empty string")) ;
+       }
+           return Validation.success(Integer.parseInt(anInt));
+
+    }
+
+   static Validation<Exception,Boolean> isValidName(String aName) {
+
+       Try1<String, Boolean,Exception> t =  n -> {
+           if (Strings.isNullOrBlank.f(n)) {
+               throw new IllegalArgumentException("A name is required");
+           } else if (Strings.contains.f("red").f(n)) {
+               return true;
+           } else {
+               return false;
+           }
+       };
+        return Try.f(t).f(aName);
+
+   }
+
 
 }
